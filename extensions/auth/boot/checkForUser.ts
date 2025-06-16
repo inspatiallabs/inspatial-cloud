6928c03e4c55f035b019e8eeddcad37e2a156f08
Eli Veffer <eli@vef.dev>
2025-06-16 21:29:42 +0300

    cleanup

--- extensions/auth/boot/checkForUser.ts
+++ extensions/auth/boot/checkForUser.ts
@@ -1,6 +1,4 @@
 import type { InCloud } from "#/inspatial-cloud.ts";
-import ColorMe from "#/utils/color-me.ts";
-import { inLog } from "#/in-log/in-log.ts";
 
 async function checkForUser(app: InCloud) {
   const { orm } = app;
@@ -30,7 +28,6 @@ async function checkForUser(app: InCloud) {
 }
 
 function promptForUser() {
-  const subject = "Create User";
   let firstName: string | null = "InSpatial";
   let lastName: string | null = "Admin";
   let email: string | null = "admin@user.com";
@@ -41,51 +38,6 @@ function promptForUser() {
     email,
     password,
   };
-  while (!firstName) {
-    firstName = prompt(ColorMe.fromOptions("First Name:", {
-      color: "brightCyan",
-    }));
-    if (!firstName) {
-      inLog.warn("First name cannot be empty", subject);
-    }
-  }
-  while (!lastName) {
-    lastName = prompt(ColorMe.fromOptions("Last Name:", {
-      color: "brightCyan",
-    }));
-    if (!lastName) {
-      inLog.warn("Last name cannot be empty", subject);
-    }
-  }
-  while (!email) {
-    email = prompt(ColorMe.fromOptions("Email:", { color: "brightCyan" }));
-    if (!email) {
-      inLog.warn("Email cannot be empty", subject);
-    }
-  }
-  while (!password) {
-    password = prompt("Password:");
-    if (!password) {
-      inLog.warn("Password cannot be empty", subject);
-    }
-  }
-  const user = {
-    firstName,
-    lastName,
-    email,
-    password,
-  };
-
-  inLog.info(
-    `User: ${ColorMe.fromOptions(user.firstName, { color: "green" })} ${
-      ColorMe.fromOptions(
-        user.lastName,
-        { color: "green" },
-      )
-    } ${ColorMe.fromOptions(user.email, { color: "green" })}`,
-    subject,
-  );
-  return user;
 }
 
 export default checkForUser;
