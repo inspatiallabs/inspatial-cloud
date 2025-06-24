9839b96442a0432500e07403e3ff43f452658ad6
Eli Veffer <eli@vef.dev>
2025-06-24 09:19:45 +0300

    wip: progress checkpoint

--- extensions/email/settingsTypes/emailSettings.ts
+++ extensions/email/settingsTypes/emailSettings.ts
@@ -39,17 +39,4 @@ export const emailSettings: SettingsType<EmailSettings> = new SettingsType<
       ],
     },
   ],
-  roles: [{
-    roleName: "basic",
-    permission: {
-      view: true,
-      modify: true,
-      fields: {
-        defaultSendAccount: {
-          view: false,
-          modify: false,
-        },
-      },
-    },
-  }],
 });
