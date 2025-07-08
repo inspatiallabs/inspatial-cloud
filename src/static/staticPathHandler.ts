cbec15c888fbfc3515ba33ded17b1cc33d8bdea0
Eli Veffer <eli@vef.dev>
2025-07-08 10:53:10 +0300

    Update staticPathHandler.ts

--- src/static/staticPathHandler.ts
+++ src/static/staticPathHandler.ts
@@ -4,7 +4,7 @@ export const staticFilesHandler: PathHandler = {
   name: "staticFiles",
   description: "Static files handler",
   // Match all paths that do not equal /api or /ws
-  match: /^\/(?!api|ws)$/,
+  match: /^\/(?!api$|ws$).*/,
   async handler(inCloud, inRequest, inResponse) {
     return await inCloud.static.serveFile(inRequest, inResponse);
   },
