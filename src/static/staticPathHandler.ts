20f6eb1e4849e5c65deaaebbe8593d803ddc1e33
Eli Veffer <eli@vef.dev>
2025-07-26 11:17:34 +0300

    refactor: fallback to static files handler if no match found in path handlers

--- src/static/staticPathHandler.ts
+++ src/static/staticPathHandler.ts
@@ -3,8 +3,9 @@ import type { PathHandler } from "~/serve/path-handler.ts";
 export const staticFilesHandler: PathHandler = {
   name: "staticFiles",
   description: "Static files handler",
-  // Match all paths that do not equal /api or /ws
-  match: /^\/(?!api$|ws$).*/,
+  // Match all paths since this is the last handler and is a fallback
+  // It will handle all requests that do not match any other handler
+  match: /.*/,
   async handler(inCloud, inRequest, inResponse) {
     return await inCloud.static.serveFile(inRequest, inResponse);
   },
